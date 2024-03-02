ac3d0807e50dc824fea15f1e19b7f8ba7ee36ffc
rupali <rupalisinghkumar12@gmail.com>
2024-03-02 15:45:42 +0530

    deleted manual folders of leadGen

--- src/main/java/com/leanplatform/MentorshipPlatform/services/CustomForm/FormResponseService.java
+++
@@ -1,9 +0,0 @@
-package com.leanplatform.MentorshipPlatform.services.CustomForm;
-
-
-import com.leanplatform.MentorshipPlatform.dto.CustomForm.GenericResponseObject;
-import com.leanplatform.MentorshipPlatform.dto.CustomForm.SubmitFormRequestObject;
-
-public interface FormResponseService {
-    GenericResponseObject submitForm(SubmitFormRequestObject submitFormRequestObject);
-}
